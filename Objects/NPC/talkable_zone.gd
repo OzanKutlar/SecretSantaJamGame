6c1af4338a24a6844a44e34dc81cf545db2f6176
Ozan <ozankutlar@protonmail.com>
2024-12-22 19:14:41 +0300

    Fixed bug

--- Objects/NPC/talkable_zone.gd
+++ Objects/NPC/talkable_zone.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 func _ready():
 	connect("body_entered", _on_body_entered)
+	connect("body_exited", _on_body_exited)
 
 
 func _on_body_entered(body):
@@ -11,4 +12,12 @@ func _on_body_entered(body):
 			if(self.get_parent().isTalkable):
 				body.ableToTalk = true;
 				body.talkTo = self.get_parent();
+		
+
+func _on_body_exited(body):
+		if body is CharacterBody2D:
+			print("Player entered NPC radius of " + self.get_parent().npc_name);
+			if(self.get_parent().isTalkable):
+				body.ableToTalk = false;
+				body.talkTo = null;
 		
